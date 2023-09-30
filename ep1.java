5756ed85c35cb1d4f3a3cc3cd7af9bcc3a611cd3
armando-vidal <armandoamvidal@gmail.com>
2023-09-30 19:41:04 -0300

    Extração de infos das linhas completas

--- ep1.java
+++ ep1.java
@@ -10,6 +10,10 @@ public class ep1{
     private static int count_COM = 0;
     private static int count_ln = 0;
     private static int act = 0;
+    private static int x = 0;
+    private static int y = 0;
+    private static int value_x = 0;
+    private static int value_y = 0;
     public static void main(String args[]) throws IOException{
 
         Path path = Paths.get("./programas/01.txt");
@@ -17,22 +21,37 @@ public class ep1{
         List <String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 
         while(act == 0){
+            System.out.println(count_ln);
             //Conta o nº de comandos COM
             if(lines.get(count_ln).equals("COM")){
+                System.out.println("Linha COM");
                 count_COM++;
+            }
             //Interrompe o while na última linha
-            } else if(lines.get(count_ln).equals("SAIDA")){
+            if(lines.get(count_ln).equals("SAIDA")){
+                System.out.println("Linha SAIDA");
                 act = 1;
             }
-            //Identifica uma linha que se inicia com 'X'
+            //Identifica uma linha que se inicia com 'X' ou 'Y'
             if(Character.compare(lines.get(count_ln).charAt(0), 'X') == 0){
-                System.out.println(lines.get(count_ln));
+                System.out.println("Linha X");
+                value_x = Integer.parseInt(lines.get(count_ln).replaceAll("[^0-9]", ""));
+                System.out.println("value_x = "+ value_x);
+                x = value_x;
+                System.out.println("valor de X = " + x);
+            } else if(Character.compare(lines.get(count_ln).charAt(0), 'Y') == 0){
+                System.out.println("Linha Y");
+                value_y = Integer.parseInt(lines.get(count_ln).replaceAll("[^0-9]", ""));
+                System.out.println("value_y = " + value_y);
+                y = value_y;
+                System.out.println("valor de Y = " + y);
             }
             count_ln++;
         }
         
-        System.out.println( ((Object)lines.get(0)).getClass().getSimpleName() );
-        System.out.println(lines);
-        System.out.println(count_COM);
+        //System.out.println( ((Object)lines.get(0)).getClass().getSimpleName() );
+        //System.out.println(lines);
+        //System.out.println(count_COM);
+        System.out.println(x + " " +  y);
     }
 }
